0ee655b5f5d3e17e383c679643a9304f9423f217
Braden Filler <bradenfiller13@gmail.com>
2016-02-18 09:19:54 -0700

    Messaging commited

---
+++ models/message.go
@@ -0,0 +1,11 @@
+package main
+
+type Message struct{
+	To User
+	From User
+	TheMessage string
+}
+
+func (this *Message) NewMessage(to, from User, theMessage string){
+	return message{To: to, From: from, TheMessage: theMessage}
+}
