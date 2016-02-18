c137ea4bdde666caa41a75428261ddf95f3daeaf
anogowski <anogowski.neumont@gmail.com>
2016-02-18 09:20:43 -0700

    placeholders for CRUD functions

--- models/user.go
+++ models/user.go
@@ -1,16 +1,13 @@
 package models
 
-import (
-	
-)
-
 type User struct {
 	userName string
+	email    string
 	password string
-	userId string
-	games []Game
+	userId   string
+	games    []Game
 	messages []string
-	friends []User
+	friends  []User
 }
 
 func (this *User) InitUser(user_name string, pass string) {
@@ -20,14 +17,38 @@ func (this *User) InitUser(user_name string, pass string) {
 	this.userId = string(1)
 }
 
-func (this *User) AddGame(game Game)	{
-	this.games = append(this.games,game)
+func (this *User) AddGame(game Game) {
+	this.games = append(this.games, game)
 }
 
-func (this *User) AddFriend(friend User){
+func (this *User) AddFriend(friend User) {
 	this.friends = append(this.friends, friend)
 }
 
-func (this *User) AddMessage(message string){
+func (this *User) AddMessage(message string) {
 	this.messages = append(this.messages, message)
+}
+
+func (this *User) UpdateEmail(mail string) {
+
+}
+
+func (this *User) UpdatePassword(pass string) {
+
+}
+
+func (this *User) UpdateGame(game Game) {
+
+}
+
+func (this *User) DeleteMessage(message string) {
+
+}
+
+func (this *User) DeleteGame(game Game) {
+
+}
+
+func (this *User) DeleteFriend(friend User) {
+
 }
