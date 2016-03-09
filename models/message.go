001f17b13c4abe1da9c140974239a2a79484574b
anogowski <anogowski.neumont@gmail.com>
2016-03-09 08:49:55 -0700

    DAL almost done

--- models/message.go
+++ models/message.go
@@ -1,11 +1,12 @@
 package models
 
-type Message struct{
-	To User
-	From User
+type Message struct {
+	To         User
+	From       User
 	TheMessage string
+	Timestamp  Time
 }
 
-func NewMessage(to, from User, theMessage string)*Message{
+func NewMessage(to, from User, theMessage string) *Message {
 	return &Message{To: to, From: from, TheMessage: theMessage}
 }
